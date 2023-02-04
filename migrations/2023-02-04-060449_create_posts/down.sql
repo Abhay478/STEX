296b7827482a91fedc013c4db228d05460bda599
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2023-02-04 12:40:23 +0530

    Setup

---
+++ migrations/2023-02-04-060449_create_posts/down.sql
@@ -0,0 +1,10 @@
+-- This file should undo anything in `up.sql`
+
+drop table posts;
+drop table users;
+drop table post_links;
+drop table badges;
+drop table post_history;
+drop table votes;
+drop table comments;
+drop table tags;
