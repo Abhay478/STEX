04cc86b72a66d04c9b6f2ac98d8f8e0c369985ca
kst164 <kartheek@tammana.org>
2023-03-05 00:46:47 +0530

    Frontend: remove unused code

--- Frontend/lib/utils/preview.dart
+++ Frontend/lib/utils/preview.dart
@@ -2,39 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../models/question.dart';
 
-Widget questionPreviewCard(BuildContext context, Post question) {
-  final String title = question.title ?? '';
-  final String tags = question.tags ?? '';
-  final String score = (question.score ?? '').toString();
-  return ListTile(
-    leading: Text(score, style: Theme.of(context).textTheme.headlineSmall),
-    title: Text(title, style: Theme.of(context).textTheme.titleLarge),
-    //subtitle: Text(tags, style: Theme.of(context).textTheme.titleSmall),
-    subtitle: Row(
-      children: [
-        Text(tags, style: Theme.of(context).textTheme.titleSmall),
-        const Spacer(),
-        Text(question.creationDateShort(), style: Theme.of(context).textTheme.titleSmall),
-      ]
-    ),
-    onTap: () => context.push('/question/${question.id}'),
-  );
-}
-
-Widget questionPreviewList(BuildContext context, List<Post>? questions) {
-  if (questions == null) {
-    return const Text('Loading...');
-  } else {
-    return
-    Container(
-      padding: const EdgeInsets.all(10),
-      child: ListView(
-        children: questions.map((question) => questionPreviewCard(context, question)).toList(), 
-      )
-    );
-  }
-}
-
 class _QuestionPreviewCard extends StatelessWidget {
   const _QuestionPreviewCard({required this.question});
 
